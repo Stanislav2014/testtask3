0642c36001767461edfcc7918fd48c8a7a817196
Stanislav2014 <35069509+Stanislav2014@users.noreply.github.com>
2018-09-13 21:44:55 +0300

    Update index.php

--- web/public/index.php
+++ web/public/index.php
@@ -1,25 +1,58 @@
 <?php
-require_once('class.php');
-    try {
-         /**
-          * Соединяемся с базой данных @test
-          */
+$dbcon = new PDO('mysql:host=mysql;dbname=test;charset=utf8', 'dev', 'dev');
+$dbcon->exec("DROP TABLE `book_autor`, `books`, `autors`");
+
+$commands = array('CREATE TABLE `books`
+( 
+    `id` integer NOT NULL PRIMARY KEY AUTO_INCREMENT ,
+    `book_name` varchar(32) NOT NULL
+)Engine=InnoDB DEFAULT CHARSET=utf8'
+,
+'CREATE TABLE `autors`
+( 
+    `id` integer NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `autor_name` varchar(32) NOT NULL
+)Engine=InnoDB DEFAULT CHARSET=utf8'
+,
+'CREATE TABLE book_autor
+(   
+    book_id integer NOT NULL,
+    autor_id integer NOT NULL,
+       FOREIGN KEY (book_id) REFERENCES books(id),
+       FOREIGN KEY (autor_id) REFERENCES autors(id)
+)Engine=InnoDB DEFAULT CHARSET=utf8'
+);
+
+foreach($commands as $command)
+  {
+    $dbcon->exec($command);
+}
+
+$autors= ["Иванов","Петров","Муров","Емелин","Бармалеев","Желов","Мармин","Кудактаев","Миллионов","Мамедов"];
+$books= ["Рецепты","Введение в профессию PHP","СУБД от и до","Программируй всегда и везде","Си тебе по силам",
+"Жизнь","Заметки программиста","Куда идти и что делать?","Миллион мне по силам","Вот и все"];
 
 
-        $dbcon = new PDO('mysql:host=mysql;dbname=test', 'dev', 'dev');
+  $stmt = $dbcon->prepare( "INSERT INTO `books`(book_name) values (?) ");
+   for ($i =0; $i < 10;$i++)
+    {
+        $stmt->execute(["$books[$i]"]);
+    }
+    $stmt = $dbcon->prepare( "INSERT INTO `autors`(autor_name) values (?) ");
+   for ($i =0; $i < 10;$i++)
+    {
+        $stmt->execute(["$autors[$i]"]);
+    }
+    $stmt = $dbcon->prepare( "INSERT INTO `book_autor`(book_id,autor_id) values (?,?) ");
+   for ($i =0; $i < 20;$i++)
+    {
+        $stmt->execute([rand(1,10),rand(1,10)]);    
+    }
 
-        /** 
-         * Создаем объект класса @Init    
-         */
+    $result = $dbcon->query("SELECT b.book_name, count(b.id) as cnt 
+    FROM books as b,book_autor as ab 
+    WHERE b.id=ab.book_id GROUP BY b.id HAVING cnt>2");
     
-        $sql = new Init($dbcon);
-
-
-        /** 
-         * Вызываем метод @get класса @Init для получения данных из таблицы @test    
-         */
-
-        $sql->get();
-    } catch(PDOException $e) {
-        echo 'Ошибка: ' . $e->getMessage();
+    foreach($result as $row) {
+        echo $row['book_name']."  "."написана"."  ".$row['cnt']."  "."авторами <br />\n";
     }
